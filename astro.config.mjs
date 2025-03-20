8be2fcb1e21a064c3b6662be173d736634a0d7c4
KJ Roelke <kroelke@choctawnation.com>
2025-03-20 09:50:21 -0500

    finish migration to astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://choctaw-nation.github.io',
+    base: 'casino-onyx-gift-event',
+    build: {
+        assets: 'dist',
+    },
+});
